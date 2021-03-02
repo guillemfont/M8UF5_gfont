0abae0f958d96a2859128f32e94ebf606e3f9ee8
guillemfont <73248044+guillemfont@users.noreply.github.com>
2021-03-02 15:13:43 +0100

    Delete M8UF5AC41_gfont directory

--- M8UF5AC41_gfont/js/saludarusuari.js
+++
@@ -1,9 +0,0 @@
-function enviarSalutacio() {
-  var nom = document.querySelector("#nom").value;
-  var cognom = document.querySelector("#cognom").value;
-
-  var mostrar = document.getElementById("mostrar");
-  mostrar.innerHTML = `
-    <p>Hola ${nom} ${cognom}, GRÀCIES PER OMPLIR EL FORMULARI </p>
-  `;
-}
