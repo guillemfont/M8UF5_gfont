8c5981d1418aa9f0af9c32e51e3157e4564ae450
guillemfont <73248044+guillemfont@users.noreply.github.com>
2021-03-02 15:15:21 +0100

    M08-JavaScript-GuillemFont

---
+++ M8UF5AC40_gfont/js/veuremes.js
@@ -0,0 +1,11 @@
+//Funció per desplegar més text
+function veureMes () {
+
+    //Desplega el text extra
+    var desplegar = document.getElementById("mestext");
+    desplegar.style.display = "inline";
+
+    //Oculta l'enllaç
+    var ocultar = document.getElementById("link");
+    ocultar.style.display = "none";
+}
---
+++ M8UF5AC42_gfont/js/canviarBombetes.js
@@ -0,0 +1,14 @@
+//Funció per canviar a l'imatge del llum apagat
+function canviaImatge(elem) {
+    elem.style.backgroundImage = "url(img/llumon.gif)"
+}
+
+//Funció per canviar a l'imatge del llum trencat
+function trencaImatge(elem) {
+    elem.style.backgroundImage = "url(img/llumbreak.gif)"
+}
+
+//Funció per tornar a l'imatge del llum encés
+function tornaImatge(elem) {
+    elem.style.backgroundImage = "url(img/llumoff.gif)"
+}
